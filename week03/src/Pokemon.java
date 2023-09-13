a13e2e3129987e00898422404c59826fb3f3dfb9
inhadeepblue <deepblue@inha.ac.kr>
2023-09-13 15:02:19 +0900

    week03 v05 overloading

--- week03/src/Pokemon.java
+++ week03/src/Pokemon.java
@@ -9,19 +9,23 @@ public class Pokemon {
     // 리턴타입이 없으며 클래스의 이름과 같다
     Pokemon(){
         System.out.println("포켓몬 객체 생성(기본 생성자)!");
+        this.hp = 100;
     }
 
     // constructor overloading
     // 생성자 오버로딩
     Pokemon(String name){
+        this(name, 1); // 항상 생성자 첫 번째 줄에 있어야 함
         System.out.println("포켓몬 객체 생성(매개변수 1개 생성자)!");
-        this.name = name;
+//        this.name = name;
+//        this.level = 1;
+//        this.hp = 100;
     }
-    Pokemon(String name, int level, int hp) {
-        System.out.println("포켓몬 객체 생성(매개변수 3개 생성자)!");
+    Pokemon(String name, int level) {
+        System.out.println("포켓몬 객체 생성(매개변수 2개 생성자)!");
         this.name = name;
         this.level = level;
-        this.hp = hp;
+        this.hp = 100;
     }
 
     // alt+insert
--- week03/src/PokemonGame.java
+++ week03/src/PokemonGame.java
@@ -1,8 +1,13 @@
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon squirtle = new Pokemon("꼬부기", 1, 50);
+        Pokemon squirtle = new Pokemon("꼬부기", 1);
         Pokemon pikachu = new Pokemon("피카츄");
         Pokemon charizard = new Pokemon();
+
+        System.out.println(pikachu.getName()
+                + "의 체력은 " + pikachu.getHp()
+                + "이고 레벨은 " + pikachu.getLevel()
+                + "입니다");
 
         squirtle.evolve(squirtle);
         System.out.println(squirtle.getName());
@@ -11,14 +16,13 @@ public class PokemonGame {
 //        pikachu.name = "피카츄";
 //        charizard.name = "리자몽";
 
-        //pikachu.setName("피카츄");
         charizard.setName("리자몽");
 
         pikachu.setLevel(23);
         pikachu.setHp(100);
 
         charizard.setLevel(36);
-        charizard.setHp(800);
+        //charizard.setHp(800);
 
         System.out.println(charizard.getName() + "의 체력은 " + charizard.getHp() + "이고 레벨은 " + charizard.getLevel() + "입니다");
         charizard.evolve(charizard);
