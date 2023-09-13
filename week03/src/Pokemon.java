283a4f65defe21a94a420c7ca8d3316587a20cdf
inhadeepblue <deepblue@inha.ac.kr>
2023-09-13 15:31:02 +0900

    week03 v09

--- week03/src/Pokemon.java
+++ week03/src/Pokemon.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Pokemon {
     // fields, 멤버 변수, 속성
     // 캡슐화
@@ -81,7 +83,16 @@ public class Pokemon {
     // methods, 멤버 함수, 행위
     void evolve(Pokemon pokemon){
         pokemon.level = pokemon.level + 1;
-        System.out.println(pokemon.name + "의 레벨이 " + pokemon.level + "으로 증가!");
+        // escape sequence를 이용한 출력 형태 조정
+        //System.out.println("\t" + pokemon.name + "의 레벨이\n\t" + pokemon.level + "으로 증가!");
+
+        // jdk15부터 지원하는 Text block과 replace 메서드(메서드 체이닝 기법) 사용
+        String texts = """
+                    name의 레벨이
+                    level으로 증가!
+                """.replace("level", pokemon.level + "")
+                .replace("name", pokemon.name);
+        System.out.println(texts);
     }
     void attack(){
         System.out.println(this.name + "이(가) 광역 도발 공격을 시전합니다");
--- week03/src/PokemonGame.java
+++ week03/src/PokemonGame.java
@@ -1,48 +1,66 @@
+import java.util.ArrayList;
+
 public class PokemonGame {
     public static void main(String[] args) {
-        System.out.println(Pokemon.getPokemonCount());
-        Pokemon squirtle = new Pokemon("꼬부기", 1);
-        Pokemon pikachu = new Pokemon("피카츄");
-        System.out.println(Pokemon.getPokemonCount());
+        ArrayList<Pokemon> pokemons = new ArrayList<>();
         Pokemon charizard = new Pokemon();
-        //System.out.println(Pokemon.pokemonCount);
-        System.out.println(Pokemon.getPokemonCount());
+        Pokemon pikachu = new Pokemon("피카츄");
+        Pokemon squirtle = new Pokemon("꼬부기", 1);
 
-        System.out.println(pikachu.getName()
-                + "의 체력은 " + pikachu.getHp()
-                + "이고 레벨은 " + pikachu.getLevel()
-                + "입니다");
+        pokemons.add(pikachu);
+        pokemons.add(squirtle);
+        pokemons.add(charizard);
 
-        squirtle.evolve(squirtle);
-        System.out.println(squirtle.getName());
-        squirtle.evolve(squirtle);
-
-//        pikachu.name = "피카츄";
-//        charizard.name = "리자몽";
-
-        charizard.setName("리자몽");
-
-//        pikachu.setLevel(23);
-//        pikachu.setHp(500);
-        pikachu.setLevel(23).setHp(500);  // method chaining.
+        System.out.println(pokemons.get(1).getName());
+        pokemons.get(2).setName("리자몽");
+        pokemons.get(2).attack(pikachu);
+        System.out.println(pokemons.size());
+        pokemons.get(0).attack();
 
 
-        charizard.setLevel(36);
-
-        System.out.println(charizard.getName() + "의 체력은 " + charizard.getHp() + "이고 레벨은 " + charizard.getLevel() + "입니다");
-        charizard.evolve(charizard);
-        pikachu.evolve(pikachu);
-        pikachu.evolve(pikachu);
-        pikachu.setName("라이츄");
-        System.out.println(pikachu.getName());
-        pikachu.evolve(pikachu);
-
-        System.out.println(pikachu.getName()
-                + "의 체력은 " + pikachu.getHp()
-                + "이고 레벨은 " + pikachu.getLevel()
-                + "입니다");
-
-        charizard.attack(squirtle);
-        squirtle.attack();
+//        System.out.println(Pokemon.getPokemonCount());
+//        Pokemon squirtle = new Pokemon("꼬부기", 1);
+//        Pokemon pikachu = new Pokemon("피카츄");
+//        System.out.println(Pokemon.getPokemonCount());
+//        Pokemon charizard = new Pokemon();
+//        //System.out.println(Pokemon.pokemonCount);
+//        System.out.println(Pokemon.getPokemonCount());
+//
+//        System.out.println(pikachu.getName()
+//                + "의 체력은 " + pikachu.getHp()
+//                + "이고 레벨은 " + pikachu.getLevel()
+//                + "입니다");
+//
+//        squirtle.evolve(squirtle);
+//        System.out.println(squirtle.getName());
+//        squirtle.evolve(squirtle);
+//
+////        pikachu.name = "피카츄";
+////        charizard.name = "리자몽";
+//
+//        charizard.setName("리자몽");
+//
+////        pikachu.setLevel(23);
+////        pikachu.setHp(500);
+//        pikachu.setLevel(23).setHp(500);  // method chaining.
+//
+//
+//        charizard.setLevel(36);
+//
+//        System.out.println(charizard.getName() + "의 체력은 " + charizard.getHp() + "이고 레벨은 " + charizard.getLevel() + "입니다");
+//        charizard.evolve(charizard);
+//        pikachu.evolve(pikachu);
+//        pikachu.evolve(pikachu);
+//        pikachu.setName("라이츄");
+//        System.out.println(pikachu.getName());
+//        pikachu.evolve(pikachu);
+//
+//        System.out.println(pikachu.getName()
+//                + "의 체력은 " + pikachu.getHp()
+//                + "이고 레벨은 " + pikachu.getLevel()
+//                + "입니다");
+//
+//        charizard.attack(squirtle);
+//        squirtle.attack();
     }
 }
