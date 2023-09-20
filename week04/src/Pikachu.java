f4bef8881e92572869b679eeded2c6f43567d44f
inhadeepblue <deepblue@inha.ac.kr>
2023-09-20 14:03:58 +0900

    week04 v18

--- week04/src/Pikachu.java
+++ week04/src/Pikachu.java
@@ -4,6 +4,7 @@ public class Pikachu extends Pokemon{
         this.level = 1;
         this.setHp(35);
         this.name = "피카츄";
+        this.flyable = new NoFly();  // Association, Has-a (Composition)
     }
 
     @Override
--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -2,7 +2,7 @@
 public class PokemonGame {
     public static void main(String[] args) {
         Pikachu pikachu = new Pikachu();
-        pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 하늘을 나는 방식을 변경
+        //pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 하늘을 나는 방식을 변경
         pikachu.performFly();
         /*
         피카츄이(가) Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Flyable.fly()" because "this.flyable" is null
