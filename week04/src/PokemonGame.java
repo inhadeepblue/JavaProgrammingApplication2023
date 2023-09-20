104c5c21d5750773ad5027d4be606d94dc52bdab
inhadeepblue <deepblue@inha.ac.kr>
2023-09-20 15:29:29 +0900

    week04 v21

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class PokemonGame {
     public static void main(String[] args) {
@@ -18,6 +19,34 @@ public class PokemonGame {
             Charizard charizard = new Charizard(wings);
         }else{
             System.out.println("여기는 영원히 실행 안됩니다");
+        }
+
+        // 플레이어 포켓몬스터 선택
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3)리자몽 : ");
+        int pokemonPick = scanner.nextInt();
+        if(pokemonPick == 1){
+            Pikachu p1 = new Pikachu(new NoFly());
+        }else if(pokemonPick == 2){
+            Squirtle p2 = new Squirtle(new NoFly());
+        }else if(pokemonPick == 3){
+            Charizard p3 = new Charizard(new Wings());
+        }else {
+            System.out.println("정상적인 값이 아닙니다!");
+        }
+
+        int menu;
+        while(true){
+            System.out.print("\t1) 전투   2) 도망   3) 종료 : ");
+            menu = scanner.nextInt();
+            if(menu == 1){
+
+            }else if(menu == 2){
+
+            }else{
+                System.out.println("게임을 종료합니다.");
+                break;
+            }
         }
     }
 }
