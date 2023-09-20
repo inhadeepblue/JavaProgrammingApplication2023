6853b7bdc2b7a9864348c0ffb457f0c8555a5e31
inhadeepblue <deepblue@inha.ac.kr>
2023-09-20 14:00:26 +0900

    week04 v17

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -2,6 +2,12 @@
 public class PokemonGame {
     public static void main(String[] args) {
         Pikachu pikachu = new Pikachu();
+        pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 하늘을 나는 방식을 변경
         pikachu.performFly();
+        /*
+        피카츄이(가) Exception in thread "main" java.lang.NullPointerException: Cannot invoke "Flyable.fly()" because "this.flyable" is null
+	at Pokemon.performFly(Pokemon.java:18)
+	at PokemonGame.main(PokemonGame.java:5)
+         */
     }
 }
