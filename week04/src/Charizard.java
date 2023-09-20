ceb4cd6b9640ea40ad3fb9afb84f943ab499cddf
inhadeepblue <deepblue@inha.ac.kr>
2023-09-20 13:57:56 +0900

    week04 v16

---
+++ week04/src/Charizard.java
@@ -0,0 +1,25 @@
+public class Charizard extends Pokemon{
+    public Charizard() {
+        System.out.println("자식 클래스(리자몽)의 기본 생성자");
+        this.level = 36;
+//      this.setLevel(36);
+        this.setHp(78);
+        this.name = "리자몽";
+    }
+
+    public Charizard(int level, int hp, String name) {
+        super(level, hp, name);  // 부모 클래스의 매개변수 생성자를 호출
+        System.out.println("자식 클래스(리자몽)의 매개변수 생성자");
+    }
+
+
+    @Override
+    public void attack() {
+        //super.attack();
+        System.out.println(this.name + "이(가) 광역 화염 공격을 시전합니다");
+    }
+
+    public void type(){
+        System.out.println(this.name + " 화염 속성!!");
+    }
+}
---
+++ week04/src/PokemonGame.java
@@ -0,0 +1,7 @@
+
+public class PokemonGame {
+    public static void main(String[] args) {
+        Pikachu pikachu = new Pikachu();
+        pikachu.performFly();
+    }
+}
