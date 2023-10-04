6470f01a005a18108bb4e95100918309dd407090
inhadeepblue <deepblue@inha.ac.kr>
2023-10-04 15:03:15 +0900

    week06 v31

--- week06/src/generic/ListDemo02.java
+++ week06/src/generic/ListDemo02.java
@@ -8,10 +8,16 @@ import java.util.List;
 public class ListDemo02 {
     public static void main(String[] args) {
         List<String> skills = new ArrayList<>();
+        List<Integer> specialAttackRate = new ArrayList<>();
         skills.add("화염방사");
         skills.add("용의숨결");
         skills.add("할퀴기");
+        specialAttackRate.add(90);
+        specialAttackRate.add(60);
+        specialAttackRate.add(40);
         for(String skill : skills)
             System.out.println(skill);
+        for (Integer rate : specialAttackRate)
+            System.out.println(rate);
     }
 }
