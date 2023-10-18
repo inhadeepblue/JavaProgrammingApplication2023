d576a21abb31a5e174e2aa8167c0854ea6e1a54a
inhadeepblue <deepblue@inha.ac.kr>
2023-10-18 14:28:37 +0900

    week08 v41

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -32,16 +32,21 @@ public class PokemonGame {
         try{
             Pokemon player = null;  // 추상클래스의 변수 선언은 가능 (upcasting 용)
             Scanner scanner = new Scanner(System.in);
-            System.out.print("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3) 리자몽 : ");
-            int pokemonPick = scanner.nextInt();
-            if(pokemonPick == 1){
-                player = new Pikachu(new NoFly());
-            }else if(pokemonPick == 2){
-                player = new Squirtle(new NoFly());
-            }else if(pokemonPick == 3){
-                player = new Charizard(new Wings());
-            }else {
-                System.out.println("정상적인 값이 아닙니다!");
+            while(true){
+                System.out.print("포켓몬을 고르세요.\n1) 피카츄   2) 꼬부기   3) 리자몽 : ");
+                int pokemonPick = scanner.nextInt();
+                if(pokemonPick == 1){
+                    player = new Pikachu(new NoFly());
+                    break;
+                }else if(pokemonPick == 2){
+                    player = new Squirtle(new NoFly());
+                    break;
+                }else if(pokemonPick == 3){
+                    player = new Charizard(new Wings());
+                    break;
+                }else {
+                    System.out.println("정상적인 값이 아닙니다!");
+                }
             }
 
             int menu, skillMenu;
