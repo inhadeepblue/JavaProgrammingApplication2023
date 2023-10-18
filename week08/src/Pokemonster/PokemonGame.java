862bbd4e4a1e1aed6898a70938dafa1e12ec3107
inhadeepblue <deepblue@inha.ac.kr>
2023-10-18 14:30:56 +0900

    week08 v42

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -74,6 +74,9 @@ public class PokemonGame {
         }catch (NullPointerException err) {
             System.out.println("플레이어 객체가 생성되지 않았습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
+        }catch (IndexOutOfBoundsException err){
+            System.out.println("존재하지 않는 스킬입니다. 기술 범위를 벗어남.");
+            System.out.println("예외 내용 : " + err.getMessage());
         }catch (Exception err){
             System.out.println("예외가 발생했습니다.");
             System.out.println("예외 내용 : " + err.getMessage());
