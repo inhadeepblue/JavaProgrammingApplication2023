e5dee607e027b67801afcb0bd12b4a2ab086d0c5
inhadeepblue <deepblue@inha.ac.kr>
2023-10-18 15:09:47 +0900

    week08 v44

--- week08/src/Pokemonster/PokemonGame.java
+++ week08/src/Pokemonster/PokemonGame.java
@@ -54,13 +54,21 @@ public class PokemonGame {
                 System.out.print("\t1) 전투   2) 도망   3) 종료 : ");
                 menu = scanner.nextInt();
                 if(menu == 1){
-                    //System.out.print("전투 기술 1) " + player.skills[0] + "   2) " +  player.skills[1] + "   3) " +  player.skills[2] + " : ");
-                    System.out.print("전투 기술 1) " + player.skills.get(0) + "   2) " +  player.skills.get(1) + "   3) " +  player.skills.get(2) + " : ");
-                    skillMenu = scanner.nextInt();
-                    //player.attack(enemy, scanner.next());
-                    //player.attack(enemy, player.skills[skillMenu-1]);
-                    player.attack(enemy, skillMenu);
-                    enemy.attack(player, (int)(Math.random() * 3)+1);
+                    while(true){
+                        //System.out.print("전투 기술 1) " + player.skills[0] + "   2) " +  player.skills[1] + "   3) " +  player.skills[2] + " : ");
+                        System.out.print("전투 기술 1) " + player.skills.get(0) + "   2) " +  player.skills.get(1) + "   3) " +  player.skills.get(2) + " : ");
+                        skillMenu = scanner.nextInt();
+                        if (skillMenu <= player.skills.size())
+                        {
+                            //player.attack(enemy, scanner.next());
+                            //player.attack(enemy, player.skills[skillMenu-1]);
+                            player.attack(enemy, skillMenu);
+                            enemy.attack(player, (int)(Math.random() * 3)+1);
+                            break;
+                        }else {
+                            System.out.println("메뉴에서 공격 기술을 선택하세요");
+                        }
+                    }
                 }else if(menu == 2){
 
                 }else if(menu == 3){
